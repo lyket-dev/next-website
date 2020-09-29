e4c6ee94f4398dd013f8ae9258bef20de4715528
Irene Oppo <i.oppo@datocms.com>
2020-09-29 14:08:13 +0200

    documentjs?

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,11 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import flush from "styled-jsx/server";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const styles = flush();
+    return { ...initialProps, styles };
   }
 
   render() {
