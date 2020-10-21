22ed478f4f42c1811549a316ae4bf63cd691fbee
Irene Oppo <i.oppo@datocms.com>
2020-10-21 12:09:28 +0200

    sitemap

---
+++ nextjs_sitemap_generator.js
@@ -0,0 +1,19 @@
+const sitemap = require("nextjs-sitemap-generator");
+
+sitemap({
+  alternateUrls: {
+    en: "https://www.lyket.dev"
+  },
+  baseUrl: "https://lyket.dev",
+  pagesDirectory: __dirname + "/pages",
+  targetDirectory: "out/",
+  nextConfigPath: __dirname + "/next.config.js",
+  sitemapStylesheet: [
+    {
+      type: "text/sass",
+      styleFile: "/styles/main.sass"
+    }
+  ]
+});
+
+console.log(`✅ sitemap.xml generated!`);
