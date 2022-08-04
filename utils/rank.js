264513ba6f180aada1c346b7c55347ea6a80ce52
Irene Oppo <i.oppo@datocms.com>
2022-08-04 19:25:06 +0200

    fix

--- utils/rank.js
+++ utils/rank.js
@@ -1,12 +1,12 @@
 export async function rankLikeButtonsByNamespace({ namespace, type }) {
   try {
     const url = namespace
-      ? `${process.env.apiBaseUrl}/v1/rank/${type}-buttons/${namespace}?limit=500`
-      : `${process.env.apiBaseUrl}/v1/rank/${type}-buttons?limit=500`;
+      ? `${process.env.apiBaseUrl}/v1/rank/${type}-buttons/${namespace}?limit=10`
+      : `${process.env.apiBaseUrl}/v1/rank/${type}-buttons?limit=10`;
 
     const response = await fetch(url, {
       headers: {
-        authorization: `Bearer ${process.env.lyketPublicApiKey}`,
+        authorization: `Bearer ${process.env.lyketResetApiKey}`,
       },
     });
 
