67ee9b99904d40c116425c50d79a520b813a1ed0
Irene Oppo <i.oppo@datocms.com>
2026-09-12 12:39:03 +0200

    docs: extract shared Provider/button props into CommonButtonProps components

---
+++ components/CommonButtonProps.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import Link from "next/link";
+import {
+  DisableSessionIdDisclaimer,
+  RecaptchaCookieNote,
+} from "components/DisableSessionIdDisclaimer";
+
+export function ProviderRequiredProps() {
+  return (
+    <li>
+      <strong>apiKey</strong>: <strong>string</strong> - You can get your
+      public API key by registering on{" "}
+      <a href="https://app.lyket.dev">Lyket</a>.
+    </li>
+  );
+}
+
+export function ProviderOptionalProps() {
+  return (
+    <>
+      <li>
+        <p>
+          <strong>
+            theme: Record&lt;'colors' | 'fonts' , Record&lt;string,
+            string&gt;&gt;
+          </strong>{" "}
+          - Allows you to change the default buttons colors and fonts. It
+          doesn't apply to all templates. Read more about it in the{" "}
+          <Link href="#styling">Styling buttons</Link> section at the end of
+          this document.
+        </p>
+      </li>
+      <li>
+        <p>
+          <strong>recaptchaSiteKey: string</strong> - If you enabled reCAPTCHA
+          in the private area's user settings, you need to provide your
+          reCAPTCHA public key, otherwise your buttons will result as
+          unauthorized. Read more in the{" "}
+          <Link href="#recaptcha">reCAPTCHA</Link> section at the end of this
+          document.
+          <RecaptchaCookieNote />
+        </p>
+      </li>
+      <li>
+        <p>
+          <strong>disableSessionId: boolean</strong> <strong>Default: false</strong>{" "}
+          - If set to <em>true</em> Lyket won't store a unique session ID for
+          your visitors making them anonymous. Lyket will then discriminate
+          visitors (to tell if they already liked a button or not) only based
+          on the IP address, writing no cookie or local storage entry.
+          <DisableSessionIdDisclaimer />
+        </p>
+      </li>
+    </>
+  );
+}
+
+export function ButtonIdRequiredProp() {
+  return (
+    <li>
+      <strong>id</strong>: <strong>string</strong> - The API uses the ID to
+      find a button. It should be unique for namespace. It accepts an
+      alphanumeric string with maximum 50 characters.
+    </li>
+  );
+}
+
+export function NamespaceOptionalProp() {
+  return (
+    <li>
+      <p>
+        <strong>namespace</strong>: <strong>string</strong> - Giving a
+        namespace is useful to keep buttons organised, and can be used to
+        fetch statistics. Check the API docs for more information.
+      </p>
+    </li>
+  );
+}
+
+export function HideCounterOptionalProp() {
+  return (
+    <li>
+      <p>
+        <strong>hideCounterIfLessThan</strong>: <strong>number</strong> - You
+        may want to hide the counter if you are not getting enough feedback.
+        Specify the number of votes/claps/likes you want to receive before
+        showing the counter.
+      </p>
+    </li>
+  );
+}
--- pages/docs/react/like-button-react.jsx
+++ pages/docs/react/like-button-react.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import {
-	DisableSessionIdDisclaimer,
-	RecaptchaCookieNote,
-} from "components/DisableSessionIdDisclaimer";
+	ProviderRequiredProps,
+	ProviderOptionalProps,
+	ButtonIdRequiredProp,
+	NamespaceOptionalProp,
+	HideCounterOptionalProp,
+} from "components/CommonButtonProps";
 import { QuickReactDocsMenu } from "components/QuickDocsMenu";
 import Code from "components/Code";
 import Head from "next/head";
@@ -164,49 +167,11 @@ ReactDOM.render(
 								</Code>
 								<h6>Required props</h6>
 								<ul>
-									<li>
-										<strong>apiKey</strong>: <strong>string</strong> - You can
-										get your public API key by registering on{" "}
-										<a href="https://app.lyket.dev">Lyket</a>.
-									</li>
+									<ProviderRequiredProps />
 								</ul>
 								<h6>Optional props</h6>
 								<ul>
-									<li>
-										<p>
-											<strong>
-												theme: Record&lt;'colors' | 'fonts' , Record&lt;string,
-												string&gt;&gt;
-											</strong>{" "}
-											- Allows you to change the default buttons colors and
-											fonts. It doesn't apply to all templates. Read more about
-											it in the <Link href="#styling">Styling buttons</Link>{" "}
-											section at the end of this document.
-										</p>
-									</li>
-									<li>
-										<p>
-											<strong>recaptchaSiteKey: string</strong> - If you enabled
-											reCAPTCHA in the private area's user settings, you need to
-											provide your reCAPTCHA public key, otherwise your buttons
-											will result as unauthorized. Read more in the{" "}
-											<Link href="#recaptcha">reCAPTCHA</Link> section at the
-											end of this document.
-											<RecaptchaCookieNote />
-										</p>
-									</li>
-									<li>
-										<p>
-											<strong>disableSessionId: boolean</strong>{" "}
-											<strong>Default: false</strong> - If set to <em>true</em>{" "}
-											Lyket won't store a unique session ID for your visitors
-											making them anonymous. Lyket will then discriminate
-											visitors (to tell if they already liked a button or not)
-											only based on the IP address, writing no cookie or local
-											storage entry.
-											<DisableSessionIdDisclaimer />
-										</p>
-									</li>
+									<ProviderOptionalProps />
 								</ul>
 							</section>
 
@@ -243,32 +208,12 @@ export BlogPost = ({ title, content }) => {
 								</div>
 								<h6>Required props</h6>
 								<ul>
-									<li>
-										<strong>id</strong>: <strong>string</strong> - The API uses
-										the ID to find a button. It should be unique for namespace.
-										It accepts an alphanumeric string with maximum 50
-										characters.
-									</li>
+									<ButtonIdRequiredProp />
 								</ul>
 								<h6>Optional props</h6>
 								<ul>
-									<li>
-										<p>
-											<strong>namespace</strong>: <strong>string</strong> -
-											Giving a namespace is useful to keep buttons organised,
-											and can be used to fetch statistics. Check the API docs
-											for more information.
-										</p>
-									</li>
-									<li>
-										<p>
-											<strong>hideCounterIfLessThan</strong>:{" "}
-											<strong>number</strong> - You may want to hide the counter
-											if you are not getting enough feedback. Specify the number
-											of votes/claps/likes you want to receive before showing
-											the counter.
-										</p>
-									</li>
+									<NamespaceOptionalProp />
+									<HideCounterOptionalProp />
 									<li>
 										<p>
 											<strong>component</strong>:{" "}
